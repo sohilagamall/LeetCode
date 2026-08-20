dbb0b05ffd408b7373a5df3180099c37a932d461
sohila gamal <157078525+sohilagamall@users.noreply.github.com>
2026-08-20 22:35:01 +0300

    [387] [Time Beats: 100.00%] [Memory Beats: 88.09%] - LeetPush

--- 387-First-Unique-Character-in-a-String.cpp
+++ 387-First-Unique-Character-in-a-String.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        map<char,int> mp;
-        for(int i=0; i<s.size() ; ++i){
-            mp[s[i]]++;
+        int freq[26] = {};
+        for(int i=0; i <s.size(); ++i){
+            freq[s[i] - 'a']++;
         }
-        
-        for(int i=0; i<s.size() ; ++i){
-            if(mp[s[i]] == 1)
-                return i;
+        for(int i=0; i < s.size(); ++i){
+            if(freq[s[i] - 'a'] == 1) return i;
         }
         return -1;
     }
